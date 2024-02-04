1d3421b0202c4ec9b936413205e6da8198ad558d
Maksim Parfenenka <138598823+makszeus@users.noreply.github.com>
2024-02-05 01:13:35 +0300

    Update source.cpp

--- cpp-quiz/135/src/source.cpp
+++ cpp-quiz/135/src/source.cpp
@@ -2,8 +2,8 @@
 #include <map>
 
 int main() {
-    std::map<bool, int> mb = {{1,2},{3,4},{5,0}};
+    std::map <bool, int> mb = {{1,2},{3,4},{5,0}};
     std::cout << mb.size();
-    std::map<int, int> mi = {{1,2},{3,4},{5,0}};
+    std::map <int, int> mi = {{1,2},{3,4},{5,0}};
     std::cout << mi.size();
 }
